c47a70b32ab1326f885829567c78f2734724974d
bsatheeshkumar <satheeshkumar@flexicontact.com>
2019-03-22 19:15:59 +0530

    Pages added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,84 @@
 $(function(){
     console.log("document loaded");
-    $('#content').load('pages/agents.html');
+    $('#content').load('pages/getting.html');
    
-    $("#index").click(function(){
+    $("#cwGetting").click(function(){
         $('#content').empty();
-        $("#content").load("pages/agents.html");
+        $("#content").load("pages/getting.html");
       });
     
     
-      $('#presence').click(function(){
-           console.log('presence button clicked');
+      $('#cwOutcome').click(function(){
+           console.log('outcome clicked');
             $('#content').empty();
-            $('#content').load('pages/skills.html');     
+            $('#content').load('pages/outcome.html');     
         });
+
+        $("#cwAudit").click(function(){
+            $('#content').empty();
+            $("#content").load("pages/audit.html");
+        });
+        
+        
+          $('#cwPhone').click(function(){
+              
+                $('#content').empty();
+                $('#content').load('pages/phone.html');     
+            });
+
+            $("#cwQueues").click(function(){
+                $('#content').empty();
+                $("#content").load("pages/queues.html");
+              });
+            
+            
+              $('#cwCallHistory').click(function(){
+                   
+                    $('#content').empty();
+                    $('#content').load('pages/callhistory.html');     
+                });
+
+                $("#cwLoginLogout").click(function(){
+                    $('#content').empty();
+                    $("#content").load("pages/login.html");
+                  });
+                
+                
+                  $('#cwBusinessHours').click(function(){
+                      
+                        $('#content').empty();
+                        $('#content').load('pages/businesshours.html');     
+                    });
+
+                    $("#cwPrompts").click(function(){
+                        $('#content').empty();
+                        $("#content").load("pages/prompts.html");
+                      });
+                    
+                    
+                      $('#cwWorkflows').click(function(){
+                          
+                            $('#content').empty();
+                            $('#content').load('pages/workflows.html');     
+                        });
+
+                        $('#cwSkills').click(function(){
+                           
+                             $('#content').empty();
+                             $('#content').load('pages/skills.html');     
+                         });
+
+                         $('#cwStatusCodes').click(function(){
+                           
+                             $('#content').empty();
+                             $('#content').load('pages/statuscode.html');     
+                         });
+
+                         $('#cwUsers').click(function(){
+                            console.log('presence button clicked');
+                             $('#content').empty();
+                             $('#content').load('pages/users.html');     
+                         });
     
         $('.sidebar-menu-list').click(function () {
             console.log('Sidebar menu list clicked');
